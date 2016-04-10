766980beaa7faffb2da4ba10c5cca17bb5437149
jellycool <2821416402@qq.com>
2016-04-10 17:55:29 +0800

    change some bootstrap views

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get('/example1', function(req, res) {
+  res.render('example1');
+});
+
 module.exports = router;
